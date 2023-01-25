b5fcad582aef332bda96480ab1f7eba19e8d4924
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-24 23:08:47 -0300

    Terminado-06

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,11 +4,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	let num1=document.getElementById("txtIdNumeroUno").value;
-	num1=parseInt(num1);
-	let num2=document.getElementById("txtIdNumeroDos").value;
-	num2=parseInt(num2);
+	let num1=parseInt(document.getElementById("txtIdNumeroUno").value);
+	
+	let num2=parseInt(document.getElementById("txtIdNumeroDos").value);
+	
 	let resultado=num1+num2;
+	
 	alert("La suma es: "+resultado);
 }
 
