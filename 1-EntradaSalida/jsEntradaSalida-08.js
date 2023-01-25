b662f4870fe41d6bb7581be61e89a633b4754a62
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-25 17:37:57 -0300

    Terminado-08

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,9 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let num1=parseInt(document.getElementById("txtIdNumeroDividendo").value);
+	
+	let num2=parseInt(document.getElementById("txtIdNumeroDivisor").value);
+	
+	alert("El resto es: "+(num1%num2))
 }
