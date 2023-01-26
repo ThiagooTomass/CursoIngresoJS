d36e6ce19090e79c79445ab8f7ef424da5c54656
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-26 20:25:20 -0300

    Terminado-03

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,10 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad=parseInt(document.getElementById("txtIdEdad").value);
+	if(edad>=18){
+		alert("Usted es mayor")
+	}else{
+		alert("Usted es menor")
+	}
 
 }//FIN DE LA FUNCIÓN
