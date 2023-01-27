6156cd14ea3a72e6e45cc2ba87a533622b533610
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-26 21:21:07 -0300

    Terminado-07

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,12 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
 
+	let edad=parseInt(document.getElementById("txtIdEdad").value);
+	let estado=document.getElementById("estadoCivil").value;
+	if(edad<=17 && edad!=estado){
+alert("Es muy pequeño para NO ser soltero.")
+	}else{
+		alert("Usted tiene "+edad+" años y su estado civil es: "+estado)
+	}
 
 }//FIN DE LA FUNCIÓN
