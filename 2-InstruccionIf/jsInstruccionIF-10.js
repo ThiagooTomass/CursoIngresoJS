cda0fc228e111774e0cb3c134faf42df2a0782f3
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-26 23:23:48 -0300

    Terminado-10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let numero = Math.round(Math.random()*(10-1)+1);
+
+	if(numero==9 || numero==10){
+alert("EXCELENTE, "+numero)
+	}else{
+		if(numero>=4&&numero<=8){
+alert("APROBO, "+numero)
+		}else{
+			alert("Vamos, la proxima se puede, "+numero)
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
