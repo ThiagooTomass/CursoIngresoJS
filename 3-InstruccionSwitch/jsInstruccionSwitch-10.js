829b4700028050cdb2aa26d1bb1d29d8f6cd6273
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-28 20:08:15 -0300

    Terminado-10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,34 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacion =txtIdEstacion.value;
+	var destino =txtIdDestino.value;
+	switch(estacion){
+		case "Invierno":
+			switch(destino){
+				case "Bariloche":alert("Se viaja");break;
+				case "Cataratas":
+				case "Mar del plata":
+				case "Cordoba":alert("No se viaja");}break;
+		
+		case "Verano":
+			switch(destino){
+				case "Mar del plata":
+				case "Cataratas":alert("Se viaja");break;
+				case "Cordoba":
+				case "Bariloche":alert("No se viaja");}break;
+		case "Otoño":
+			switch(destino){
+				case "Bariloche":
+				case "Cataratas":
+				case "Mar del plata":
+				case "Cordoba":alert("Se viaja");}break;
+		case "Primavera":
+			switch(destino){
+			case "Cataratas":
+			case "Mar del plata":
+			case "Cordoba":alert("Se viaja");break;
+			case "Bariloche":alert("No se viaja");}
+	}
+}
 
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
