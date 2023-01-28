1d2af30f298622264c536c5e61c88ba6287fc41c
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-28 16:49:10 -0300

    Terminado-01

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,8 +1,18 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mes =txtIdMes.value;
+	
+	switch(mes){
+		case "Enero":
+			alert("que comiences bien el año!!!.");break;
+		case "Marzo":
+			alert("a clases!!!.");break;
+		case "Julio":
+			alert("se vienen las vacaciones!!!.");break;
+		case "Diciembre":
+			alert("Felices fiesta!!!.");break;
+	}
 
 
 
