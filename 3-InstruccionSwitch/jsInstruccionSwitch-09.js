08b38b7399387ab036a1b951f2e01c730f448f3b
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-28 19:34:35 -0300

    Terminado-09

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,31 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+
+	var estacion =txtIdEstacion.value;
+	var destino =txtIdDestino.value;
+	switch(estacion){
+		case "Invierno":
+			switch(destino){
+				case "Bariloche":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+((15000*20/100)+15000));break;
+				case "Cataratas":
+				case "Cordoba":	alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000-(15000*10/100)));break;
+				case "Mar del plata":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000-(15000*20/100)));break;
+			}break;
+		case "Verano":
+			switch(destino){
+				case "Bariloche":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000-(15000*20/100)));break;
+				case "Cataratas":
+				case "Cordoba":	alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000+(15000*10/100)));break;
+				case "Mar del plata":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000+(15000*20/100)));break;
+			}break;
+		case "Otoño":
+		case "Primavera":
+			switch(destino){
+				case "Bariloche":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000+(15000*10/100)));break;
+				case "Cataratas":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000+(15000*10/100)));break;
+				case "Cordoba":	alert("El precio del voleto a "+destino+" en "+estacion+" es de "+15000);break;
+				case "Mar del plata":alert("El precio del voleto a "+destino+" en "+estacion+" es de "+(15000+(15000*10/100)));break;
+			}
+	}
 
 }//FIN DE LA FUNCIÓN
