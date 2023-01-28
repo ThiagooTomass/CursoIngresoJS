0bca92fb975084f7ae357484a11483f581140c3b
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-28 17:41:56 -0300

    Terminado-03

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mes =txtIdMes.value;
+	switch(mes){
+		case "Febrero":
+			alert("Este mes no tiene más de 29 días.");break;
+		default:alert("Este mes tiene 30 o más días");
+	}
 	
 	
 
