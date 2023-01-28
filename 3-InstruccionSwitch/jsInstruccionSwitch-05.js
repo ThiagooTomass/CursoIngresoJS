fa22cc4acbaccc1ccd114e23b831a9b7518974a1
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-28 18:02:34 -0300

    Terminado-06

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,14 @@
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
-	
 
-
+let hora=parseInt(document.getElementById("txtIdHora").value);
+switch(hora){
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:alert("Es de mañana");break;
+	default:alert("No es de mañana");
+}
 
 }//FIN DE LA FUNCIÓN
