43d18591effe9ac7c45085115a179e92beb30b4b
Thiago Ramos <thiagotomasramos@gmail.com>
2023-01-28 17:55:36 -0300

    Terminado-04

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,17 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mes =txtIdMes.value;
 	
+	switch(mes){
 	
+	case "Febrero":alert(mes+" tiene 28 dias");break;
+
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":alert(mes+"tiene 30 dias");break;
 
 
-
+	default:alert(mes+" tiene 31 dias");
+	}
 }//FIN DE LA FUNCIÓN
